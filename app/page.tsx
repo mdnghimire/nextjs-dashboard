30155f9c2ca97a5655b31aa7c54051e11801256b
mdnghimire <madan.ghimire@xrdig.com>
2024-04-02 07:57:03 +0545

    fixes issue

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel..
           </p>
           {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" /> */}
           <div className={styles.shape}></div>
