acdba5e2260d830c589f5ca4b492671a3f402f93
mdnghimire <madan.ghimire@xrdig.com>
2024-04-14 21:59:31 +0545

    add meta data on app

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,10 +3,15 @@ import { lusitana } from '@/app/ui/fonts';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import Table from '@/app/ui/invoices/table';
 import Search from '@/app/ui/search';
+import { Metadata } from 'next'
 
 import Pagination from '@/app/ui/invoices/pagination';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page({
   searchParams,
