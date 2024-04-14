421565cbbba5c6958c1651315c13e354b33dd6d3
mdnghimire <madan.ghimire@xrdig.com>
2024-04-14 21:18:59 +0545

    fix minor issue

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -63,6 +63,8 @@ export default function LoginForm() {
                 required
                 minLength={6}
               />
+              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+
               <label
                 htmlFor="password"
                 className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3"
